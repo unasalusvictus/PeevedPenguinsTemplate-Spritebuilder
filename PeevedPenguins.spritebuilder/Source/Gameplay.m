500112eebdc11f5801c433dd1fef320e691e0cbe
Tom Mayclin <spokenfore@Toms-MacBook-Pro.local>
2014-10-30 07:12:36 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -40,7 +40,7 @@
         _mouseJointNode.position = touchLocation;
         
         //setup a spring joint between the mouseJointNode and the catapultarm
-        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34, 138) restLength:0.f stiffness:0.f damping:150.f];
     }
 }
 
