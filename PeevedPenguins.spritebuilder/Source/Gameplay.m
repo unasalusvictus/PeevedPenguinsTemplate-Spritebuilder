8c8217162ec82ad4bea2a563a2d2f53ddddea32a
Tom Mayclin <spokenfore@toms-mbp.socal.rr.com>
2014-10-30 18:19:02 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -16,6 +16,8 @@
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
+    CCNode *_currentPenguin;
+    CCPhysicsJoint *_penguinCatapultJoint;
 }
 
 // is called when CCB file has completed loading
@@ -41,6 +43,15 @@
         
         //setup a spring joint between the mouseJointNode and the catapultarm
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+        
+        //create a penguin from the ccb-file
+        _currentPenguin = [CCBReader load:@"Penguins"];
+        
+        //initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
+        CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34,138)];
+        
+        //transform the world position to the node space to which the penguin will be added (_physicsNode)
+        [_p]
     }
 }
 
